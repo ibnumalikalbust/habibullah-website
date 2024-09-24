ed83f6b24018e8e509f9c6439fbcfc265198ed0d
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-24 15:01:31 +0700

    mengubah lokasi fazzdev css

--- default.php
+++ default.php
@@ -12,7 +12,7 @@
 	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://fazzdev.com/vendor/local/2024.css">
+	<link rel="stylesheet" href="https://fazzdev.com/css/2024.css">
 </head>
 
 <body class="bg-dark">
